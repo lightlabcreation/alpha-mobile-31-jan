589ea2464c7db07c53530fda03434fb5ab0113d6
lightlabcreation <lightlabcreation@gmail.com>
2026-01-31 15:08:28 +0530

    Update dbConnect.jshhhh
    fggh

--- Config/dbConnect.js
+++ Config/dbConnect.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const dbConnect = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MongoDB_URL);
     console.log("Database Connected Successfully ✅✅✅✅");
   } catch (error) {
     console.error("Database error ❌❌❌❌", error.message);
